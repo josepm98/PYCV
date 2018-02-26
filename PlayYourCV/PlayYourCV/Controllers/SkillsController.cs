39cebde571f51ba985400f527227a91324996aa4
AresLeo <ares.leonardo90@gmail.com>
2018-02-26 19:30:08 +0100

    Controladores y Creacion de pag Login
    Controladores y vistas de skills,  logros y Login.
    Creacion de la pagina login con un nuevo viewstart

---
+++ PlayYourCV/PlayYourCV/Controllers/SkillsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PlayYourCV.Controllers
+{
+    public class SkillsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+    }
+
+}
