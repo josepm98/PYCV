4a1e7ea77d06c4e5fdc9d93b0233829f37e0ac67
Joshua <joshua_440@hotmail.com>
2018-02-28 17:33:33 +0100

    controller de models
    bbddcontroler para que hereden el resto de dicha clase

---
+++ PlayYourCv/PlayYourCv/Models/UsuarioBBDDController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using PlayYourCV.Models;
+
+namespace PlayYourCv.Models
+{
+    public class UsuarioBBDDController : BBDDController<Usuario>
+    {
+        public UsuarioBBDDController()
+        {
+            _table = "Usuarios";
+        }
+
+        public override List<Usuario> ToListModel(MySqlDataReader rdr)
+        {
+            List<Usuario> list = new List<Usuario>();
+            while (rdr.Read())
+            {
+
+            }
+            return list;
+        }
+
+        public override Usuario ToModel(MySqlDataReader rdr)
+        {
+            Usuario usuario = new Usuario();
+            while (rdr.Read())
+            {
+
+            }
+            return usuario;
+        }
+    }
+}
