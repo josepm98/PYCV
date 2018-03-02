ad2977a3ea74f452de824bf539d20793c95bf0f2
Joshua <joshua_440@hotmail.com>
2018-03-02 19:00:52 +0100

    ended basic idiomas
    need to do only controller

--- PlayYourCv/PlayYourCv/Controllers/IdiomasController.cs
+++ PlayYourCv/PlayYourCv/Controllers/IdiomasController.cs
@@ -18,21 +18,89 @@ namespace PlayYourCV.Controllers
         }
         public ActionResult Index()
         {
-            return View();
-        }
+            /*if (String.IsNullOrEmpty(Session["logged"] as String))
+            {
+                ViewBag.UserIsLogged = false;
+                ViewData["Title"] = "Home Page";
+                return View("Index","Login");//return to home
+            }
+            else
+            {
+                ViewBag.UserIsLogged = true;
+                ViewBag.Logged = Session["logged"] as String;
+                ViewBag.LoggedId = Session["loggedid"] as String;
 
-        public ActionResult Edit()
-        {
-            ViewData["Message"] = "Your application description page.";
+                ViewData["listaIdiomas"] = GetUserLanguages(ViewBag.LoggedId);
+                ViewData["Titulo"] ="Idiomas";
+                return View();
+            }*/
 
+            //TODO delete after testing
+            ViewData["Titulo"] = "Idiomas";
             return View();
         }
 
         public ActionResult Create()
         {
-            ViewData["Message"] = "Your contact page.";
+           
 
             return View();
+        }
+
+        // GET: Login/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Login/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Login/Delete/5
+        public ActionResult Delete(int id)
+        {
+            // TODO: Add update logic here
+            return RedirectToAction("Index");
+        }
+
+        //BBDD methods
+        public List<Contenido> GetUserLanguages(int idUser)
+        {
+            List<Contenido> list = new List<Contenido>();
+            try
+            {
+                openConn();
+                string sql =string.Format("SELECT * FROM {0} WHERE {1}=@uid",_table,_idCol);
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = sql;
+                cmd.Connection = _conn;
+                cmd.Parameters.AddWithValue("@uid", idUser);
+                cmd.Prepare();
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                list = ToListModel(rdr);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetBaseException().ToString());
+            }
+            finally
+            {
+                closeConn();
+            }
+            return list;
         }
 
         public override Contenido ToModel(MySqlDataReader rdr)
@@ -40,18 +108,31 @@ namespace PlayYourCV.Controllers
             Contenido c = new Contenido();
             if (rdr.Read())
             {
-
+                c = singleContenidoReader(rdr);
             }
-            return null;
+            return c;
         }
 
         public override List<Contenido> ToListModel(MySqlDataReader rdr)
         {
+            List<Contenido> list = new List<Contenido>();
             while (rdr.Read())
             {
-
+                list.Add(singleContenidoReader(rdr));
             }
             return null;
+        }
+		
+		private Contenido singleContenidoReader(MySqlDataReader rdr)
+        {
+            Contenido c = new Contenido();
+            c.Id = Convert.ToInt32(rdr[_idCol].ToString());
+            c.IdUsuario = Convert.ToInt32(Session["loggedid"] as String);
+            c.Hablado = rdr["Hablado"].ToString();
+            c.Leido = rdr["Leido"].ToString();
+            c.Escrito = rdr["Escrito"].ToString();
+            c.NivelGeneral = rdr["NivelGeneral"].ToString();
+            return c;
         }
     }
 }
